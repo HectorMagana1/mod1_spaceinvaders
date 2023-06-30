c4ca899996146f52aa34ff48124065231e7e4287
Hector Magana <hectormagana8423@gmail.com>
2023-06-29 20:57:07 -0600

    commit

--- script.js
+++ script.js
@@ -4,9 +4,7 @@ const unitArea = {
 };
 
 const player1Points = document.querySelector('#player1-points');
-const player2Points = document.querySelector('#player2-points');
 const player1Button = document.querySelector('button#player1');
-const player2Button = document.querySelector('button#player2');
 const timer = document.querySelector('#timer');
 const message = document.querySelector('#message');
 
@@ -26,18 +24,12 @@ const playerShip = {
 
 let alienColumns = 4;
 let alienRows = 3;
-const bulletArr = [];
-const alienArr =[];
+let bulletArr = [];
+let alienArr =[];
 let endOfTurn = false;
 let endOfGame = false;
 let alienCount = 0;
-let playerBullets = {
-    player1:0,
-    player2:0
-}
-
-let player1 = false;
-let player2 = false;
+let playerPoints = 0;
 
 let seconds = 30;
 let bulletsShot = 0;
@@ -54,10 +46,18 @@ function animate(){
     drawPlayerShip(playerShip);
     updateAliens(alienArr);   
     updateBullet();
-    player1Points.innerHTML = playerBullets.player1;
-    player2Points.innerHTML = playerBullets.player2;
+
+
+    if (alienCount==0){
+        alienColumns++
+        alienRows++
+        alienArr=[];
+        bulletArr=[];
+        createAlienArray(); 
+    }
+    player1Points.innerHTML = playerPoints;
     timer.innerHTML = seconds;
- }
+}
 
 animate(); 
 
@@ -87,12 +87,7 @@ function updateBullet(){
                 alienArr[j].hit = true;
                 bulletArr[i].hit = true;
                 alienCount--;
-                if (player1){
-                    playerBullets.player1 += Math.floor(100+(alienArr[j].xPosition*.2)+(seconds*.2)-(bulletsShot*2));
-                }
-                else if (player2){
-                    playerBullets.player2 += Math.floor(100+(alienArr[j].xPosition*.2)+(seconds*.2)-(bulletsShot*2));
-                }
+                playerPoints += Math.floor(100+(alienArr[j].xPosition*.2)+(seconds*.2)-(bulletsShot*2));
             }
         }
 
@@ -104,12 +99,16 @@ function updateBullet(){
 
 }
 
-function endGame () {
-    if (player2 && alienColumns.length<=0){
-        seconds = 30;
-        createAlienArray(alienColumns,alienRows)
-    }
-}
+// function endGame () {
+    // if (alienCount==0){
+    //     alienColumns++
+    //     alienRows++
+    //     alienArr=[];
+    //     bulletArr=[];
+    //     createAlienArray();
+    //     updateBullet();
+    // }
+// }
 
 
 function createAlienArray(columns, rows){
@@ -127,7 +126,6 @@ function createAlienArray(columns, rows){
         }
     }
     alienCount = alienArr.length;
-    // console.log(alienArr);
 }
 createAlienArray(alienColumns,alienRows);
 
@@ -168,18 +166,7 @@ document.addEventListener('keydown',function(event){
     else if (event.key === 'ArrowRight' && playerShip.xPosition+playerShip.width<canvas.width){
         playerShip.xPosition += playerShip.xPosChange;
     }
-})
-
-document.addEventListener('click',function(event){
-    if (event.target === player1Button){
-        player1 = true;
-        player2 = false;
-    }
-    else if (event.target === player2Button){
-        player2 = true;
-        player1 = false;
-    }
-})
+});
 
 document.addEventListener('keyup',function(event){
     if (event.key === ' '){    
@@ -194,4 +181,4 @@ document.addEventListener('keyup',function(event){
         bulletArr.push(bullet);
         bulletsShot++
     }
-})
+});
